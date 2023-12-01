1109ace9fdfced0f51ee003ea895fb82d6ed8aea
CyraxVip <128605688+CyraxVip@users.noreply.github.com>
2023-12-01 17:11:47 +0700

    Update twrp_a21s.mk

--- twrp_a21s.mk
+++ twrp_a21s.mk
@@ -1,38 +1,29 @@
 #
-# Copyright (C) 2022 TeamWin Recovery Project
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from TWRP common configurations
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from a21s device
 $(call inherit-product, device/samsung/a21s/device.mk)
 
-# A21s uses dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a21s
 PRODUCT_NAME := twrp_a21s
-PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Galaxy A21s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A217F
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a21snsxx-user 12 SP1A.210812.016 A217FXXSADWI1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a21snsxx/a21s:12/SP1A.210812.016/A217FXXSADWI1:user/release-keys
